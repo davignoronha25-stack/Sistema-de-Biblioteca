4e9d45a26d1ee94f36f40ed580c04726cc2eb3d9
Lavínia <laviniaoliveira8920@gmail.com>
2026-08-13 09:01:04 -0300

    adicionei a função cadastrar livros

--- biblioteca.py
+++ biblioteca.py
@@ -8,8 +8,8 @@ def carregar_livros():
     try:
         with open(ARQUIVO, "r", newline="", encoding="utf-8") as arquivo:
             leitor = csv.DictReader(arquivo)
-            for livros in leitor:
-                livros.append(livros)
+            for livro in leitor:
+                livros.append(livro)
     except FileNotFoundError:
         pass
 
@@ -18,3 +18,30 @@ def carregar_livros():
 def salvar_livros(livros):
     with open(ARQUIVO, "w", newline="", encoding="utf-8") as arquivo:
         campos = ["título", "autor", "ano", "código", "status"]
+
+        escritor = csv.DictWriter(arquivo, fieldnames=campos)
+        escritor.writeheader()
+        escritor.writerows(livros)
+
+def cadastrar_livro(livros):
+    print("\n===CADASTRAR LIVROS===")
+
+    titulo=input("Digite o título: ")
+    autor=input("Digite o nome do autor: ")
+    ano=input("Digite o ano de publicação: ")
+    codigo=input("Digite o código/ISBN: ")
+
+    livro = {
+        "Título": titulo,
+        "Autor": autor,
+        "Ano": ano,
+        "Código": codigo,
+        "Status": "Disponivel"
+
+    }
+
+    livros.append(livro)
+    salvar_livros(livros)
+
+    print("\nLivro cadastro com sucesso!")
+    return livro
